788bb0c0db175e7ff95d66be7b25a776e1fedb40
Alexander Heo <jikster712@gmail.com>
2021-03-22 13:08:27 -0700

    Typist package installed, flavor text animated

--- client/components/infotext.jsx
+++ client/components/infotext.jsx
@@ -1,35 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Typist from 'react-typist'
 import styled from 'styled-components'
 
-const Infotext = props => {
-  const { pokemon, species } = props
-  const gen = species.generation.name.split('-')[1].toUpperCase()
-  const txt = species.flavor_text_entries[0].flavor_text
-  const text = txt.replace('\n', ' ').replace('\f', ' ')
-  return (
-    <Info>
-      <div className="window">
-      	<div className="nameContainer">
-          <span className="name">{`${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)}`}</span>
-          <span className="number">no. {pokemon.id}</span>
-          <span className="gen">Gen. {gen}</span>
-        </div>
-        <div className="infoContainer">
-          <div className="stats">
-            <span className="statname">height:</span>
-            <span className="stat">{pokemon.height}</span>
-          </div>
-          <div className="stats">
-            <span className="statname">weight:</span>
-            <span className="stat">{pokemon.weight}</span>
-          </div>
-        </div>
-        <div className="textContainer">
-          <span className="text">{text}</span>
-        </div>
-      </div>
-    </Info>
-  )
+class Infotext extends Component {
+	state = {
+	  flavorTexts: [],
+	  flavorDisplay: '',
+	  flavorSet: false
+	}
+
+	componentDidMount = () => {
+	  const fT = []
+	  this.props.species.flavor_text_entries.forEach(x => {
+	    if (x.language.name === 'en') {
+	      fT.push(x.flavor_text)
+	    }
+	  })
+	  this.setState({
+	    flavorTexts: fT,
+	    flavorDisplay: fT[0],
+	    flavorSet: true
+	  })
+	}
+
+	render() {
+	  const { pokemon, species } = this.props
+	  const { flavorDisplay, flavorSet } = this.state
+	  const gen = species.generation.name.split('-')[1].toUpperCase()
+	  return (
+	    <Info>
+	      <div className="window">
+	        <div className="nameContainer">
+	          <span className="name">{`${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)}`}</span>
+	          <span className="number">no. {pokemon.id}</span>
+	          <span className="gen">Gen. {gen}</span>
+	        </div>
+	        <div className="infoContainer">
+	          <div className="stats">
+	            <span className="statname">height:</span>
+	            <span className="stat">{pokemon.height}</span>
+	          </div>
+	          <div className="stats">
+	            <span className="statname">weight:</span>
+	            <span className="stat">{pokemon.weight}</span>
+	          </div>
+	        </div>
+	        <div className="textContainer">
+	          {
+	            flavorSet &&
+							<Typist>
+							  <Typist.Delay ms={1000} />
+							  {flavorDisplay}
+							</Typist>
+	          }
+	        </div>
+	      </div>
+	    </Info>
+	  )
+	}
 }
 
 export default Infotext
@@ -42,8 +70,6 @@ const Info = styled.div`
 	align-items: center;
 	position: relative;
 	font-family: 'VT323', monospace;
-	/* background-color: gainsboro;
-	border: 1px solid black; */
 	border-radius: 0.3rem;
 	margin: 1rem 0;
 	z-index: 2;
@@ -52,20 +78,19 @@ const Info = styled.div`
 		height: 90%;
 		display: flex;
 		flex-flow: column;
-		justify-content: center;
+		justify-content: flex-start;
 		align-items: center;
 		font-size: 1.2rem;
 		background-color: lightgreen;
 		border: 1px solid green;
 		border-radius: 0.3rem;
+		box-shadow: inset 2px 2px 4px darkgreen, inset -2px -2px 4px limegreen;
 		.nameContainer {
 			border-bottom: 1px solid black;
+			margin: 1.5rem 0 0.5rem;
 		}
 		.infoContainer {
-			width: 100%;
-			display: flex;
-			justify-content: space-evenly;
-			margin: 0.5rem 0;
+			margin-bottom: 0.5rem;
 			.stats {
 				display: flex;
 				justify-content: space-between;
@@ -90,8 +115,14 @@ const Info = styled.div`
 		}
 		.textContainer {
 			width: 70%;
-			height: 70px;
-			overflow-y: scroll;
+			height: 38px;
+			position: relative;
+			margin: 1rem;
+			overflow-y: hidden;
+			.Typist {
+				position: absolute;
+				bottom: 0;
+			}
 		}
 	}
 `
